c107c0daaeec6e1b8ee433b6b41ebbbcd0ad18d9
gecheline <a.kochoska@gmail.com>
2019-11-21 10:42:35 -0500

    init widget wrapper

--- phoetting/widgets.py
+++ phoetting/widgets.py
@@ -1,23 +1,50 @@
 import numpy as np 
 import phoebe
 from ipywidgets import *
+from IPython.display import display, clear_output
 from phoetting import utils
+import matplotlib.pyplot as plt
 
 class ParamsPlotWidget(object):
     
-    def __init__(self, names, twigs, ranges, bundle):
-        
-        self.names = names
-        self.twigs = twigs
-        self.ranges = ranges
+    def __init__(self, bundle, params):
+
+        '''
+        Parameters
+        ----------
+        bundle: phoebe.Bundle object
+            The bundle to work with in the widget
+        params: dict
+            Parameter names as phoebe twigs and their corresponding ranges
+        '''
+
+        self.twigs, self.ranges = utils.check_params(params, bundle)
         self.bundle = bundle
+        self.build_layout()
 
     def build_layout(self):
 
         # iterate over names and ranges and build and link slider/text
         # add to list of sliders/texts
         # group in HBoxes and VBoxes
-        return None
+        self.sliders = []
+        self.texts = []
+        for name, box in zip(self.twigs, self.ranges):
+            value = 0.5*(box[0]+box[1])
+            step = (box[1]-box[0])/100.
+            slider, text = self.make_slider_text(value,box[0],box[1],step,name)
+            self.sliders.append(slider)
+            self.texts.append(text)
+
+        lc_button = Button(description="Plot LC")
+        rv_button = Button(description="Plot RV")
+        self.plot = Output()
+
+        lc_button.on_click(self.on_lc_button_clicked)
+        rv_button.on_click(self.on_rv_button_clicked)
+
+        #TODO: add options for phoebe-backend and compute options (ntriangles, gridsize...)
+        
 
     @staticmethod
     def make_slider_text(value, minv, maxv, step, description):
@@ -41,40 +68,27 @@ class ParamsPlotWidget(object):
             disabled=False,
             layout=Layout(width='85px'),
         )
+
+        jslink((slider, 'value'), (text, 'value'))
         return slider, text
 
-    # @staticmethod
-    # def update_vals_in_bundle(period=period, rsum=rsum, rratio=rratio, incl=incl,
-    #                     esinw=eccen*np.sin(omega), ecosw=eccen*np.cos(omega), sma=sma, q=1.0, tratio=tratio,
-    #                     pblum=12, lc=True, rv=False):
-    #     [period, rsum, rratio, incl, esinw, ecosw, tratio, sma, q, pblum, vgamma] = [s.value for s in sliders]
-        
-    #     b['period@binary'] = period
-    #     b['incl@binary'] = incl
-    #     b['sma@binary'] = sma
-    #     b['esinw'] = esinw
-    #     b['ecosw'] = ecosw
-    #     b['teff@secondary'] = tratio*b['value@teff@primary']
-    #     b['requiv@primary'] = b['value@sma@binary']*rsum/(1.+rratio)
-    #     b['requiv@secondary'] = b['value@sma@binary']*rsum*rratio/(1.+rratio)
-    #     b['q'] = q
-    #     b['pblum@primary'] = pblum
-    #     b['vgamma'] = vgamma
 
-    #     b.run_compute(compute='legacy01')#, ntriangles=500)
+    def update_vals_in_bundle(self):
 
-    @staticmethod
-    def link(self, slider, text):
-        widgetLink = jslink((slider, 'value'), (text, 'value'))
+        for i in range(len(self.twigs)):
+            self.bundle[self.twigs[i]] = self.sliders[i].value
 
-    # @staticmethod
-    # def on_lc_button_clicked(button):
+        self.bundle.run_compute()#, ntriangles=500)
+
+
+    def on_lc_button_clicked(self, button):
         
-    #     for s in sliders:
-    #         s.observe(update_vals_in_bundle, 'value')
+        for s in self.sliders:
+            s.observe(self.update_vals_in_bundle, 'value')
         
-    #     with plot:
-    #         clear_output()
+        with self.plot:
+            clear_output()
+    
     #         plt.figure(figsize=(10,5))
     #         plt.plot(b.to_phase(b['value@times@lc@dataset']), b['fluxes@lc@model'].interp_value(times=lc[:,0]), '.', c='#008699')
     #         plt.scatter(b.to_phase(b['value@times@lc@dataset']), b['value@fluxes@lc@dataset'], c='#3f3535')
@@ -83,19 +97,20 @@ class ParamsPlotWidget(object):
     # #         plt.xlim([1730,1750])
     #         plt.show()
 
-    # @staticmethod
-    # def on_rv_button_clicked(button):
 
-    #     for s in sliders:
-    #         s.observe(update_vals_in_bundle, 'value')
-    #     with plot:
-    #         clear_output()
-    #         plt.figure(figsize=(10,5))
-    #         plt.plot(b.to_phase(b['value@times@rv@primary@model']), b['value@rvs@rv@primary@model'], '.', c='#008699')
-    #         plt.scatter(b.to_phase(b['value@times@rv@primary@dataset']), b['value@rvs@rv@primary@dataset'], c='#3f3535')
-    #         plt.xlabel('Time (JD)')
-    #         plt.ylabel('RVs (km/s)')
-    #         plt.show()
+    def on_rv_button_clicked(self, button):
+
+        for s in self.sliders:
+            s.observe(self.update_vals_in_bundle, 'value')
+        
+        with self.plot:
+            clear_output()
+            # plt.figure(figsize=(10,5))
+            # plt.plot(b.to_phase(b['value@times@rv@primary@model']), b['value@rvs@rv@primary@model'], '.', c='#008699')
+            # plt.scatter(b.to_phase(b['value@times@rv@primary@dataset']), b['value@rvs@rv@primary@dataset'], c='#3f3535')
+            # plt.xlabel('Time (JD)')
+            # plt.ylabel('RVs (km/s)')
+            # plt.show()
 
     # def display(self):
     #     return None
